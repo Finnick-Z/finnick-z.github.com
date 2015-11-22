71c2e9f59c91957c6fe55866ad88d08b07e74e4c
Finnick-Z <Finnick_Z@yeah.net>
2015-11-22 12:17:17 +0800

    ok
    ok

--- xingguangapp/js/main.js
+++ xingguangapp/js/main.js
@@ -7,12 +7,21 @@ window.onload = function(){
 	
 		var oLink = document.getElementById("link");
 		var oBtn = document.getElementById("btn")
+		var onoff = 1;
 		
 		oLink.onclick = function(ev){
-		
+			
 			var ev = ev || window.event;
-			ev.preventDefault()
-	
+			ev.preventDefault();
+			if(onoff){
+				oBtn.style.backgroundImage = "url(img/btn_bg2.png)";
+				onoff = 0;
+			}else{
+				oBtn.style.backgroundImage = "url(img/btn_bg1.png)";
+				onoff = 1;
+			}
+			
+
 		}
 
 	})()
